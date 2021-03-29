9bb5403430f3476c33289c951c0dc7e0ea7a8b79
ts-maksym.sharov <ts-maksym.sharov@rakuten.com>
2021-03-29 23:08:31 +0900

    Added output writers

---
+++ src/main/java/com/uzabase/rssreader/exception/OutputWriterNotFoundException.java
@@ -0,0 +1,7 @@
+package com.uzabase.rssreader.exception;
+
+public class OutputWriterNotFoundException extends RuntimeException {
+    public OutputWriterNotFoundException(String message) {
+        super(message);
+    }
+}
