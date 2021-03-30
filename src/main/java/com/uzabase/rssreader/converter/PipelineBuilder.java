1bb075c902692d77d6fb1339080de8f2d9267712
ts-maksym.sharov <ts-maksym.sharov@rakuten.com>
2021-03-30 21:54:31 +0900

    Implemented input and output

---
+++ src/main/java/com/uzabase/rssreader/converter/PipelineBuilder.java
@@ -0,0 +1,14 @@
+package com.uzabase.rssreader.converter;
+
+public class PipelineBuilder {
+
+    public Pipeline buildPipeline(String[] convert) {
+//        Arrays.stream(convert).
+//                map(convert -> toConverter())
+        return null;
+    }
+
+    private Object toConverter() {
+        return null;
+    }
+}
